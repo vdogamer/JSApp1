ecfe21be49cd4bd58847720b2190c1252dd9ef23
vdogamer <ctd8z@virginia.edu>
2023-02-11 13:50:53 -0500

    Added comments for cloning arrays

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const arr1 = ['one', 'string', 100, false, []];
 console.log(arr1[2]);
 
 const arr3 = [];
+
+// first way to clone an array
 arr1.forEach((ele, ind) => {
     console.log(ele);
     arr3[ind] = ele;
 })
 console.log(arr3);
 arr3[4] = "CHANGED";
+
+arr3.push('new val');
+arr3.unshift('stupid value');   // adds new value to the beginning of the array
+
+// second way to clone an array
+const arr4 = arr1.map((el)=>{
+    console.log(el);
+    return el;
+})
+console.log(arr4);
+
+// shortest way to clone an array
+const arr5 = arr1.map((x)=>x);
