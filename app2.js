5df3c89dd22de339a6f3efc0b4da7e3e2bd0fca3
vdogamer <ctd8z@virginia.edu>
2023-02-16 22:52:14 -0500

    Had to relocate the myJson function

--- app2.js
+++ app2.js
@@ -2,22 +2,28 @@
 const url = 'j1.json';
 let data = '';
 
-const localData = localStorage.
-getItem('temp');
-console.log(localData);
-
-myJson();
-// console.log(person);
-
-
-
 function myJson(){
     fetch(url)
     .then(rep => rep.text())
     .then(json => {
         data = JSON.parse(json);
         // console.log(data);
-        let str = JSON.stringify.apply(data);
-        localStorage.setItem('temp', str);
+        let str = JSON.stringify(data);
+        localStorage.setItem('temp1', str);
     })
 }
+
+const localData = localStorage.getItem('temp1');
+console.log(localData);
+if(!localData){
+   myJson(); 
+} else {
+    console.log(localData);
+}
+
+
+
+// console.log(person);
+
+
+
